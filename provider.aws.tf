f04b9181ba2302e5a2b2bc68e634b1f30c2edac3
James Woolfenden <james.woolfenden@gmail.com>
2020-06-14 19:00:18 +0100

    bump version

--- provider.aws.tf
+++ provider.aws.tf
@@ -1,13 +1,13 @@
 
 provider "archive" {
-  version = "1.3"
+  version = "~> 1.3"
 }
 
 provider "aws" {
   region  = "us-west-2"
-  version = "2.50"
+  version = "~> 2.64"
 }
 
 provider "template" {
-  version = "2.1"
+  version = "~> 2.1"
 }
