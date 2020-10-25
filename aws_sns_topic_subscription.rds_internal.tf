05bc6cd22033ba534dbd7d967ab58dca4db435d0
James Woolfenden <James.Woolfenden@gmail.com>
2020-10-25 16:33:46 +0000

    no tags on subs

--- aws_sns_topic_subscription.rds_internal.tf
+++ aws_sns_topic_subscription.rds_internal.tf
@@ -2,5 +2,4 @@ resource "aws_sns_topic_subscription" "rds_internal" {
   topic_arn = aws_sns_topic.internal.arn
   protocol  = "lambda"
   endpoint  = aws_lambda_function.rds_setup.arn
-  tags      = var.common_tags
 }
