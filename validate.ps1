ff514b58416c7d70a455cc8b967ad6a7b9239d13
James Woolfenden <james.woolfenden@gmail.com>
2021-03-19 22:12:41 +0000

    Managed by Terraform

--- validate.ps1
+++ validate.ps1
@@ -1,7 +1,11 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference ="Stop"
+terraform fmt --recursive
+Push-Location
+Set-Location example/examplea
 Remove-Item .terraform -Recurse -ErrorAction SilentlyContinue
 Remove-Item .terraform.lock.hcl -ErrorAction SilentlyContinue
 terraform init -upgrade
 terraform validate
-terraform fmt
+make valid
+Pop-Location
